99aa108f6544923bc863f683c9f20f44e8ab3bab
zacgeis <zacgeis@gmail.com>
2018-06-03 16:54:16 -0500

    Initial pass at hash table prototypes

--- ulib.h
+++ ulib.h
@@ -67,9 +67,9 @@ struct u_list_t {
 #define u_list_new(type) _u_list_new(sizeof(type))
 u_list_t *_u_list_new(size_t object_size);
 void u_list_free(u_list_t *list);
-#define u_list_prepend(type, list, val) (* (type *) _u_list_prepend(list)) = val
+#define u_list_prepend(type, list, val) (* ((type *) _u_list_prepend(list))) = val
 void* _u_list_prepend(u_list_t *list);
-#define u_list_append(type, list, val) (* (type *) _u_list_append(list)) = val
+#define u_list_append(type, list, val) (* ((type *) _u_list_append(list))) = val
 void* _u_list_append(u_list_t *list);
 void u_list_remove(u_list_t *list, u_list_node_t *node);
 int u_list_length(u_list_t *list);
@@ -80,10 +80,19 @@ u_list_node_t *u_list_node_new(size_t object_size);
 void u_list_node_free(u_list_node_t *list_node);
 u_list_node_t *u_list_node_next(u_list_node_t *cur);
 u_list_node_t *u_list_node_prev(u_list_node_t *cur);
-#define u_list_node_value_v(type, node) (* (type *) _u_list_node_value(node))
-#define u_list_node_value_p(type, node) (type *) _u_list_node_value(node)
+#define u_list_node_value_v(type, node) (* ((type *) _u_list_node_value(node)))
+#define u_list_node_value_p(type, node) ((type *) _u_list_node_value(node))
 void *_u_list_node_value(u_list_node_t *node);
 
-// HASHTABLE
+// HASH TABLE
+
+#define u_hash_table_new(type) _u_hash_table_new(sizeof(type))
+u_hash_table_t *_u_hash_table_new(size_t object_size);
+void u_hash_table_free(u_hash_table_t *table);
+#define u_hash_table_put(type, table, key, val) (* ((type *) _u_hash_table_put(table, key))) = val
+void *_u_hash_table_put(u_hash_table_t *table, char *key);
+#define u_hash_table_get_v(type, table, key) (* ((type *) _u_hash_table_get(table, key)))
+#define u_hash_table_get_p(type, table, key) ((type *) _u_hash_table_get(table, key))
+void *_u_hash_table_get(u_hash_table_t *table, char *key);
 
 #endif
